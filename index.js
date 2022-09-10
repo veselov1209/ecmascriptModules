2f9ed4658b6150c2e6c26af776c64b08809c040e
veselov1209 <veselov1209@gmail.com>
2022-09-10 12:00:40 +0300

    example with 2 functions default

---
+++ index.js
@@ -0,0 +1,8 @@
+// const y = 12345;
+import math from "./math.js";
+
+const res1 = math.sum(5,5);
+console.log(res1);
+
+const res2 = math.pow2(2,2);
+console.log(res2);
---
+++ math.js
@@ -0,0 +1,8 @@
+const x = 100;
+
+const pow2 = (base, exp) => base ** exp
+
+const sum = (x, y) => x + y
+
+
+export default {pow2, sum};
